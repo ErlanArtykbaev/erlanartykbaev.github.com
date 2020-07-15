d16cf2c7e87f78025cd6b213acfec1b2b9940174
erlan artykbaev <erik.2405@mail.ru>
2020-07-15 18:04:14 +0600

    paralax is ready. doing github section

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
-function add(l){
-  return l++;
-}
+let react = document.getElementById("react");
+let bg = document.getElementById("bg");
+let text = document.getElementById("title");
+
+window.addEventListener('scroll', function(){
+  var value = window.scrollY;
+
+  react.style.left = value * 1  + 'px';
+  bg.style.top = value * 0.40 + 'px';
+  text.style.top = value * 1 + 'px';
+})
