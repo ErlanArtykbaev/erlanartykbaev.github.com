df430287f2a979602ef6d533a328bbbb20bc4c8d
erlan artykbaev <erik.2405@mail.ru>
2020-11-21 20:45:22 +0600

    refactor: refactor code with prettier

--- js.js
+++ js.js
@@ -1,22 +1,20 @@
-let react = document.getElementById("react");
-let bg = document.getElementById("bg");
-let text = document.getElementById("title");
+let react = document.getElementById('react');
+let bg = document.getElementById('bg');
+let text = document.getElementById('title');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function() {
   var value = window.scrollY;
 
-  react.style.left = value * 1  + 'px';
-  bg.style.top = value * 0.40 + 'px';
+  react.style.left = value * 1 + 'px';
+  bg.style.top = value * 0.4 + 'px';
   text.style.top = value * 1 + 'px';
-})
+});
 
-function burger(){
-  const x = document.getElementById('myUrls')
-  if (x.style.display === "block") {
-    x.style.display = "none";
+function burger() {
+  const x = document.getElementById('myUrls');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
-
 }
-
